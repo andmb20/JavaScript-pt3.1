32adbe94b92a242669501c91a1ef3d245d728758
André <152936614+andmb20@users.noreply.github.com>
2024-08-18 17:44:27 -0300

    correcting spelling errors For.js

--- For.js
+++ For.js
@@ -1,9 +1,9 @@
 /* LAÇOS (ESTRUTURA DE REPETIÇÃO) = LOOP
 
     - FOR
-        1) INICIALIZAÇÃO -> Criar uma variavel ee colocar um valor inicial para ela
+        1) INICIALIZAÇÃO -> Criar uma variavel e colocar um valor inicial para ela
         2) CONDIÇÃO ->  Enquanto for true, o laço continuara iterando.
-                        Ele irá verificarantes de cada iteração.
+                        Ele irá verificar antes de cada iteração.
         3) EXPRESSÃO FINAL -> O que irá ocorrer a cada vez que o nosso laço der uma volta.
 
         for ([inicialização]; [condição]; [expressão final]){
