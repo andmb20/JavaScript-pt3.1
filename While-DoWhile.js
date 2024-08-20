ba62b1b39e63d9f19384556685eae3435871fe7d
andre <andremorais0000@gmail.com>
2024-08-20 20:38:12 -0300

    added while and do while

---
+++ While-DoWhile.js
@@ -0,0 +1,21 @@
+/*
+    While & Do While
+
+    While -> Verifica se é TRUE e faz
+    Do While -> Faz, depois veja se é TRUE
+
+*/
+
+let i = 0
+
+while (i < 15){
+    i++;
+    console.log(i)
+}
+
+let n = 0
+
+do {
+    n++;
+    console.log(n)
+} while (n < 10);
