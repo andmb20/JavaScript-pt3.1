e464f3fd692e88bc98800133a26b595ea09aaa88
andre <andremorais0000@gmail.com>
2024-08-22 22:09:40 -0300

    Added the function with "return"

--- Return-ForEach.js
+++ Return-ForEach.js
@@ -1,9 +1,16 @@
 const cart = [10, 244, 99, 2, 20, 33, 250]
 let finalValue = 0
 
+function discountAdded(price, discount) {
+    const result = (price * discount) / 100
+   // return result
+    console.log(result)
+
+}
+
+discountAdded(250, 7)
+
 
 cart.forEach(value => {
     finalValue = finalValue + value
 });
-
-console.log(finalValue)
