70e5b5a5aa41c25dd816d54a2ade8e8e0af0e9bb
andre <andremorais0000@gmail.com>
2024-08-22 22:23:25 -0300

    Application completed

--- Return-ForEach.js
+++ Return-ForEach.js
@@ -1,16 +1,25 @@
+/*
+
+This is a challenge that I needed to create an array and build an algorithm that would give a 10% discount when the price was higher than 30 and add the other values ​​to a single value.
+
+*/
+
 const cart = [10, 244, 99, 2, 20, 33, 250]
 let finalValue = 0
 
 function discountAdded(price, discount) {
     const result = (price * discount) / 100
-   // return result
-    console.log(result)
-
+    return result
 }
 
-discountAdded(250, 7)
-
-
 cart.forEach(value => {
-    finalValue = finalValue + value
+
+    if (value > 30) {
+        const discount = discountAdded(value, 10)
+        finalValue = finalValue + (value - discount)
+    } else {
+        finalValue += value
+    }
 });
+
+console.log(finalValue)
