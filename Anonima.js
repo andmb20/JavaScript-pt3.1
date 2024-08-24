db195b49f0f5e00b8abef8a7bb9bb37f55cad27b
andre <andremorais0000@gmail.com>
2024-08-23 22:45:06 -0300

    Added anonymous functions

---
+++ Anonima.js
@@ -0,0 +1,20 @@
+/*
+    Função anonima
+*/
+//           tipo arrow
+array.forEach(element => {
+    
+});
+
+array.forEach(function() {
+
+});
+
+//        tipo arrow
+setInterval(() => {
+    
+}, interval);
+
+setInterval(function() {
+
+}, interval);
